c2c75f9442c1454e27d29e5a88b06b06e45ca156
kikuchitko <sgwizm@icloud.com>
2018-12-14 17:45:28 +0900

    タガメット

--- FrogLoop/Assets/Scripts/Tagame.cs
+++ FrogLoop/Assets/Scripts/Tagame.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //タガメ
-public class Tagame : IEnemy {
-    void Start () {
-		
-	}
+public class Tagame : IEnemy
+{
+
+    [SerializeField]
+    private float Tagame_y;
+
+    private bool Tagame_count = true;
+
+    private Transform Tagame_Pos;
+
+    void Start()
+    {
+        Tagame_Pos=GetComponent<Transform>();
+    }
     protected override void FixedUpdate()
     {
 
+        if (Tagame_y > Tagame_Pos.localPosition.y)
+        {
+            Tagame_count = true;
+        }
+        else
+        {
+            Tagame_count = false;
+        }
+
+        if (Tagame_count == true)
+        {
+            transform.Translate(0, Tagame_y, 0);
+        }
+        if (Tagame_count == false)
+        {
+            transform.Translate(0, -Tagame_y, 0);
+        }
     }
 
     protected override void OnCollisionEnter2D(Collision2D other)
@@ -17,3 +44,4 @@ public class Tagame : IEnemy {
 
     }
 }
+
