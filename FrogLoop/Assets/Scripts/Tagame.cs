08d832d1d85db02a0a796ba967a80fc277ce9ee5
kikuchitko <sgwizm@icloud.com>
2018-12-14 18:56:10 +0900

    たがめ２

--- FrogLoop/Assets/Scripts/Tagame.cs
+++ FrogLoop/Assets/Scripts/Tagame.cs
@@ -6,37 +6,49 @@ using UnityEngine;
 public class Tagame : IEnemy
 {
 
-    [SerializeField]
+    
     private float Tagame_y;
-
-    private bool Tagame_count = true;
+    [SerializeField]
+    private float Tagame_haba;
+    [SerializeField]
+    private float  Speed;
 
     private Transform Tagame_Pos;
 
+    
     void Start()
     {
         Tagame_Pos=GetComponent<Transform>();
+        Tagame_y = transform.localPosition.y;
     }
     protected override void FixedUpdate()
     {
-
-        if (Tagame_y > Tagame_Pos.localPosition.y)
-        {
-            Tagame_count = true;
-        }
-        else
-        {
-            Tagame_count = false;
-        }
-
-        if (Tagame_count == true)
-        {
-            transform.Translate(0, Tagame_y, 0);
-        }
-        if (Tagame_count == false)
-        {
-            transform.Translate(0, -Tagame_y, 0);
-        }
+        //time += Time.deltaTime;
+        //if (time > 1)
+        //{
+        //    if (Tagame_y > Tagame_Pos.localPosition.y)
+        //    {
+        //        Tagame_count = true;
+        //        Debug.Log("ながぬま");
+        //    }
+        //    else
+        //    {
+        //        Tagame_count = false;
+        //        Debug.Log("こばやし");
+        //    }
+        //    time = 0;
+        //}
+        //Debug.Log(time);
+        //if (Tagame_count == true)
+        //{
+        //    transform.Translate(0, Tagame_Speed, 0);
+        //}
+        //if (Tagame_count == false)
+        //{
+        //    transform.Translate(0, -Tagame_Speed, 0);
+        //}
+       
+        transform.position = new Vector3(transform.position.x, Tagame_y+Tagame_haba  * Mathf.Sin(Time.time * Speed), transform.position.z);
     }
 
     protected override void OnCollisionEnter2D(Collision2D other)
