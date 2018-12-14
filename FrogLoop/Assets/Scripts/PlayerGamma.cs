5c76abef1de3fe32d01093361205e6c410bb7629
naoya-nogu <naoya-nogu10317@gmail.com>
2018-12-14 17:55:21 +0900

    左右に進む(画面基準)

--- FrogLoop/Assets/Scripts/PlayerGamma.cs
+++ FrogLoop/Assets/Scripts/PlayerGamma.cs
@@ -3,24 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //カエル
-public class PlayerGamma : IPlayerMove {
+public class PlayerGamma : IPlayerMove
+{
+    [SerializeField]
+    private float _speed;//カエルのスピード
+
+    private Vector3 _playerPos;
+    private Vector3 _mousePosition;
     void Start()
     {
-        Debug.Log("Start");
+        //Debug.Log("Start");
     }
 
-    void OnEnable()
+    void OnEnable()//
     {
-        Debug.Log("Enable");
+
+        //Debug.Log("Enable");
     }
 
-    void FixedUpdate()
+    void FixedUpdate()//ここメイン
     {
-
+        if (Input.GetMouseButton(0))
+        {
+            _mousePosition = Input.mousePosition - new Vector3(Screen.width / 2, 0, 0);
+            _playerPos = this.transform.position;           
+            Move();
+        }
     }
 
     protected override void Move()
     {
 
+        if (0 < _mousePosition.x)//プレイヤーより右側タップ
+        {
+
+            _playerPos += new Vector3(_speed, 0, 0);
+        }
+        else if (0 > _mousePosition.x)//プレイヤーより左側をタップ
+        {
+            _playerPos -= new Vector3(_speed, 0, 0);
+        }
+        this.transform.position = _playerPos;
     }
 }
