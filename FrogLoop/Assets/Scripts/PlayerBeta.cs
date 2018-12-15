b6b7a871c542c58357b65614f636ccfbdae47464
DESKTOP-CNIEST5\vantan <uyryuyr@gmail.com>
2018-12-15 16:01:41 +0900

    微妙に動きま

--- FrogLoop/Assets/Scripts/PlayerBeta.cs
+++ FrogLoop/Assets/Scripts/PlayerBeta.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 //オタマジャクシ足つき
 public class PlayerBeta : IPlayerMove {
+
+    
+
     void Start()
     {
         Debug.Log("Start");
@@ -16,11 +19,32 @@ public class PlayerBeta : IPlayerMove {
 
     void FixedUpdate()
     {
-
+       
+        if (Input.GetMouseButton(0))
+        {
+           
+            Move();
+        }
+        //ここでむーぶよびだす
     }
 
     protected override void Move()
     {
-
+        
+        float x = this.transform.position.x;
+        
+        Vector3 _mousePosittion = Input.mousePosition - new Vector3(Screen.width / 2, 0, 0);
+        Debug.Log(x);
+        if (x < _mousePosittion.x)
+        {
+            this.transform.position += new Vector3(0.01f, 0, 0);
+           
+        }else if(x > _mousePosittion.x)
+        {
+            this.transform.position -= new Vector3(0.01f, 0, 0);
+        }
+        
+       
+        //ここでかく
     }
 }
