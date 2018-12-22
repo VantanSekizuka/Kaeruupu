1635181c63842050d9ae2eef2a95f83b2d03c938
naoya-nogu <naoya-nogu10317@gmail.com>
2018-12-22 18:22:23 +0900

    床にスクリプト追加

---
+++ FrogLoop/Assets/Scripts/Ground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Contains("Player"))
+        {
+            if (collision.gameObject.GetComponent<PlayerStatus>().status == PlayerStatus.Status.GAMMA)
+            {
+                if (collision.gameObject.GetComponent<PlayerGamma>().JumpingFlag == true)
+                {
+                    collision.gameObject.GetComponent<PlayerGamma>().JumpingFlag = false;
+                }
+            }
+        }
+    }
+}
