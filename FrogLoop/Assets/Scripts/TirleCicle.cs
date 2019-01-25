b4495c136f6b1ab35ea2a4eb30416f7be12c0302
DESKTOP-CNIEST5\vantan <uyryuyr@gmail.com>
2019-01-25 17:32:23 +0900

    タイトルアニメーションとか

---
+++ FrogLoop/Assets/Scripts/TirleCicle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TirleCicle : MonoBehaviour {
+
+    [SerializeField]
+    private Image Cicle;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 ciclemove = new Vector3(0, 0, 0);
+        ciclemove.z += 0.5f;
+
+        Cicle.transform.Rotate(ciclemove); 
+
+    }
+}
