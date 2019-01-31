97a9b46b7041fd205276711fb78410aaaf6baf1b
DESKTOP-CNIEST5\vantan <uyryuyr@gmail.com>
2019-01-31 15:50:38 +0900

    魚シーン追加＆魚動くよ　こがたん

---
+++ FrogLoop/Assets/Scripts/Fish.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish :  IEnemy {
+
+    public float speed = 0.001f;
+    Vector3 fish = Vector3.zero;
+
+   
+
+	// Use this for initialization
+	void Start () {
+        fish = this.gameObject.transform.position;
+	}
+
+
+    protected override void FixedUpdate()
+    {
+        if (IPlayerMove.InWaterFlag)
+        {
+            fish.x += speed;
+            this.gameObject.transform.position = fish;
+        }
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+          
+            Debug.Log("こがたん");
+        }
+    }
+
+}
