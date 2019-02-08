e5eb3e920224fe8ee51833887b04bfeb7ffaf670
kikuchitko <sgwizm@icloud.com>
2019-02-08 17:15:37 +0900

    多が目

---
+++ FrogLoop/Assets/Scripts/hiru.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hiru : IEnemy{
+    protected override void FixedUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D other)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private float hiru_x;
+    [SerializeField]
+    private float hiru_haba;
+    [SerializeField]
+    private float Speed;
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
