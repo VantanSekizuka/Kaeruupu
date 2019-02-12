3ace44516446374da0a037ef762ee624c67e5cf2
DESKTOP-IFH7LER\trait <traitblow@gmail.com>
2019-02-13 02:16:43 +0900

    船追加

---
+++ FrogLoop/Assets/Scripts/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+    Rigidbody2D _rb;
+    [SerializeField] int clearCount;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    bool changedFlag;
+    void Update()
+    {
+        var prevFlag = changedFlag;
+        changedFlag = PlayerAlpha.FriendCount >= clearCount;
+        if (!prevFlag && changedFlag)
+        {
+            _rb.constraints = RigidbodyConstraints2D.None;
+        }
+    }
+}
