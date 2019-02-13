08df22948dc289b7da28814d90741f29c6c829b0
DESKTOP-CNIEST5\vantan <uyryuyr@gmail.com>
2019-02-13 19:01:09 +0900

    シーンとUI追加ですわよ

---
+++ FrogLoop/Assets/Scripts/ClearManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ClearManager : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite[] sprites;
+
+    [SerializeField]
+    private Image image;
+
+    [SerializeField]
+    private Text text;
+
+    private string StageText;
+    private string ClearText;
+
+	// Use this for initialization
+	void Start () {
+        StageText = "次はステージ(o･∇･o)";
+        ClearText = "ステージクリア(o･∇･o)";
+        text.text = StageText;
+	}
+	
+    public void OnNextClick()
+    {
+        
+    }
+
+    public void OnSelectClick()
+    {
+        SceneManager.LoadScene("Select");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
