dd0f29dabae00a79c8bd271783c46e1c97e8cf16
DESKTOP-CNIEST5\vantan <uyryuyr@gmail.com>
2019-02-14 20:19:26 +0900

    スタッフロール追加

---
+++ FrogLoop/Assets/StaffManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StaffManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject text;
+
+    [SerializeField]
+    private float speed = 10;
+
+    float y = -2256;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTitleClick()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (y <= 2245)
+        {
+            y += speed;
+
+            text.gameObject.transform.localPosition = new Vector3(0, y, 0);
+        }
+    }
+}
